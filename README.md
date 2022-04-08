14715d146f1331ea10a6317cf664b5d4d9e24839
Ruimin <50272957+ruimin1231@users.noreply.github.com>
2022-04-07 22:20:15 -0700

    challenge

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# surfs_up
+# Surfs_Up
+---
+### _Analyzing weather data on Python from an SQLite Database using SQLAlchemy and a Flask App_
+
+# Overview
+---
+This project consisted on analyzing weather data from Oahu, Hawaii to support a Surf and Ice-cream shop investment proposal. The analysis consisted on determining whether the seasons could affect the surf and ice-cream business. The final product was a full report and a flask application that read the data from a SQLite Database for other users to access it.
+
+# Resources
+---
+* Data resources:
+ - hawaii.sqlite
+ 
+ ### Software used:
+
+* Python
+* SQLAlchemy
+* SQLite
+* Flask
+
+## Results
+
+Statistical results of the precipitation and temperature of the two most active and important months across the years as follows:
+
+[June & December Png files](screenshots)
+
+1. As it can be seen, the average temperature for both June and December are considered good weather suitable for ice-cream (71°F and 74.0°F).
+
+2. None of the two seasons of the year show to have considerably high average precipitation. The precipitation distribution for both June and December happens to be skewed to the right meaning that it is not normally raining. 1st Quartile and median have 0mm, and the 3rd Quartile has 0.1mm which is minimum precipitation.
+
+3. It must be taken into consideration that the month of december happens to have a max precipitation of 6.4mm which is considered heavy rain. On the other hand, the max precipitation for June happens to be 4.4mm which is considered medium-high. Overall, the distribution shows that precipitation might not be enough reason to close our shop out-of-business due to weather conditions.
+
+4. Taking all this into account, June happens to be the most profitable time of the year as the distribution of the weather is more suitable for surfing and ice-cream sales.
+
+## Summary
+___
+The results were found by querying the SQLite database from the Jupyter notebook to then use the describe() function to get the statistical analysis.
+
+For more detail on the Python script and libraries used refer to the [Surfs_up](SurfsUp_Challenge.ipynb)
---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, jsonify
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Set up the database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create sesion
+session = Session(engine)
+
+#Create Flask Session
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    <hr>
+    Available routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    /api/v1.0/plot
+    ''')
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
